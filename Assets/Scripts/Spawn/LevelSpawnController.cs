b72e3dc16c00c09aee555e477ec4d3cd24f6b93e
DESKTOP-NBIU3MK\workstation <qqqq8692@naver.com>
2025-04-11 20:14:00 +0900

    no message

--- Assets/Scripts/Spawn/LevelSpawnController.cs
+++ Assets/Scripts/Spawn/LevelSpawnController.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using UnityEngine;
+
 
 public class LevelSpawnController : MonoBehaviour
 {
@@ -8,12 +10,27 @@ public class LevelSpawnController : MonoBehaviour
 
     private void Start()
     {
+        StartCoroutine(asdAsync());
+    }
+
+    private IEnumerator asdAsync()
+    {
+        while (true)
+        {
+            create();
+
+            yield return new WaitForSeconds(5f);
+        }
+    }
+
+    public void create()
+    {
         for (int i = 0; i < spawnCount; i++)
         {
             var randPoint = Random.insideUnitSphere * spawnRadius; randPoint.y = 0;
             var spawnPoint = transform.position + randPoint;
 
-             GameApplication.Instance.EntityController.Spawn<Monster, MonsterObject>(spawnEntityId, spawnPoint, Quaternion.identity);
+            GameApplication.Instance.EntityController.Spawn<Monster, MonsterObject>(spawnEntityId, spawnPoint, Quaternion.identity);
         }
     }
 }
