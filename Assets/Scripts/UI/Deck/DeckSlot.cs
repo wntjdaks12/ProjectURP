d2c2e974845515b7d5f38c0f39613ba686a1d64d
DESKTOP-NBIU3MK\workstation <qqqq8692@naver.com>
2025-04-29 23:42:40 +0900

    no message

--- Assets/Scripts/UI/Deck/DeckSlot.cs
+++ Assets/Scripts/UI/Deck/DeckSlot.cs
@@ -27,11 +27,14 @@ public class DeckSlot : View
             {
                 checkedCont.gameObject.SetActive(true);
 
+                // 히어로 생성
                 var heroData = deckViewModel.PlayerViewModel.HeroDatas[index];
                 var heroObj = GameApplication.Instance.EntityController.Spawn<Hero, HeroObject>(heroData.id, new Vector3(0, 6.937001f, 0), Quaternion.identity);
 
+                // 소환 VFX 생성
                 GameApplication.Instance.EntityController.Spawn<VFX, VFXObject>(40005, heroObj.transform.position, Quaternion.identity);
 
+                // 덱 데이터에 히어로 추가
                 deckViewModel.AddCombatDeck(heroObj.Id);
                 heroId = heroObj.Id;
             }
@@ -39,9 +42,11 @@ public class DeckSlot : View
             {
                 checkedCont.gameObject.SetActive(false);
 
+                // 히어로 삭제
                 var hero = GameApplication.Instance.GameModel.RunTimeData.ReturnData<Hero>(nameof(Hero), heroId);
                 hero.OnRemoveData();
 
+                // 덱 데이터에 히어로 제거
                 deckViewModel.RemoveCombatDeck(heroId);
                 heroId = 0;
             }
---
+++ Assets/Scripts/UI/HeroInfo/HeroInfoPanel.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroInfoPanel : View
+{
+    public override void UpdateUI()
+    {
+    }
+}
