845581b439a48055b54232705c0a5051aa6962d8
DESKTOP-NBIU3MK\workstation <qqqq8692@naver.com>
2025-05-03 01:11:04 +0900

    랜덤 이동 관련 추가

--- Assets/Scripts/Character/Hero/HeroController.cs
+++ Assets/Scripts/Character/Hero/HeroController.cs
@@ -1,5 +1,7 @@
+using System.Collections;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class HeroController : MonoBehaviour
 {
@@ -10,6 +12,8 @@ public class HeroController : MonoBehaviour
     private void Start()
     {
         heroObject.SetSkill();
+
+        //StartCoroutine(StartRandomAsync());
     }
 
     private void Update()
@@ -37,6 +41,33 @@ public class HeroController : MonoBehaviour
         }
     }
 
+    #region 이동 관련 제어
+    public IEnumerator StartRandomAsync()
+    {
+        while (true)
+        {
+            var point = RandomPoint();
+            heroObject.OnMove(point);
+
+            yield return new WaitUntil(() => (transform.position - point).sqrMagnitude < 0.1f);
+        }
+    }
+
+    private Vector3 RandomPoint()
+    {
+        NavMeshHit navMeshHit;
+
+        while (true)
+        {
+            var randCirclePos = Random.insideUnitCircle * 10;
+            var spawnPos = transform.position; spawnPos.x += randCirclePos.x; spawnPos.z += randCirclePos.y;
+            if (NavMesh.SamplePosition(spawnPos, out navMeshHit, 1f, NavMesh.AllAreas))
+            {
+                return navMeshHit.position;
+            }
+        }
+    }
+    #endregion
     #region 마나 관련 제어
 
     public void CheckMp()
