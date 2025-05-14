524e15774d3b2772f30afabcc80f0220f5cd3327
DESKTOP-NBIU3MK\workstation <qqqq8692@naver.com>
2025-05-15 05:14:28 +0900

    동전 효과 수정

--- Assets/Scripts/DropItem/DropItemTweener.cs
+++ Assets/Scripts/DropItem/DropItemTweener.cs
@@ -23,11 +23,12 @@ public class DropItemTweener : MonoBehaviour
                     vibrato: 30, // 쉐이크 횟수
                     randomness: 90, // 랜덤
                     fadeOut: true // 쉐이크 페이드 아웃
-             ));
+             )).Join(transform.DOLocalMove(new Vector3(0, 0, 0), 0.25f)); // 위에서 아래로 떨어지는 트윈스 조인
     }
 
     private void OnEnable()
     {
+        transform.localPosition = new Vector3(0, 10, 0);
         transform.rotation = Quaternion.Euler(0, Random.Range(-360f, 361f), 0);
 
         // 쉐이크 트윈스 재생
